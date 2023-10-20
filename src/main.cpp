deb0ef7f3905102b33cb33887de394c771ad84e5
Ts-sound <1564264931@qq.com>
2023-10-20 14:01:49 +0800

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,31 @@
 /**
- * This example turns the ESP32 into a Bluetooth LE mouse that continuously moves the mouse.
+ * This example turns the ESP32 into a Bluetooth LE mouse that continuously
+ * moves the mouse.
  */
 
-#include "BleMouse.h"
 #include <Arduino.h>
 #include <cJSON.h>
 
-BleMouse bleMouse;
+#include "BleMouse.h"
+#include "wifi.h"
+
+class Wifi wifi;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  bleMouse.begin();
+  Serial.println("Starting work!");
+  wifi.SetUp("T", "12345678");
 }
 
 void loop() {
-  if(bleMouse.isConnected()) {
-    Serial.println("Left click");
-    bleMouse.click(MOUSE_LEFT);
+  while (wifi.IsConnect()) {
+    auto s = wifi.ServerRead();
+    if (s != "") {
+      Serial.println("read : %s"s.c_str());
+      wifi.ServerWrite(s);
+    }
     delay(500);
 
-    Serial.println("Right click");
-    bleMouse.click(MOUSE_RIGHT);
-    delay(500);
-
-    Serial.println("Scroll wheel click");
-    bleMouse.click(MOUSE_MIDDLE);
-    delay(500);
-
-    Serial.println("Back button click");
-    bleMouse.click(MOUSE_BACK);
-    delay(500);
-
-    Serial.println("Forward button click");
-    bleMouse.click(MOUSE_FORWARD);
-    delay(500);
-
-    Serial.println("Click left+right mouse button at the same time");
-    bleMouse.click(MOUSE_LEFT | MOUSE_RIGHT);
-    delay(500);
-
-    Serial.println("Click left+right mouse button and scroll wheel at the same time");
-    bleMouse.click(MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE);
-    delay(500);
-
+    delay(50*1000);
   }
 }
